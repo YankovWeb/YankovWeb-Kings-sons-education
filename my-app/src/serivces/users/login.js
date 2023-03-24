2886adc8b68b2561cafd3f7133739027bb891682
Grigor Yankov <yankovweb@outlook.com>
2023-03-24 20:05:23 +0200

    [Future]loginServices

--- my-app/src/serivces/users/login.js
+++ my-app/src/serivces/users/login.js
@@ -1,5 +1,15 @@
-const login = () => {
-  return;
+import {auth} from "./firebase";
+
+const login = async (email, password) => {
+  try {
+    const userCredential = await auth.signInWithEmailAndPassword(
+      email,
+      password
+    );
+    return userCredential.user;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 export default login;

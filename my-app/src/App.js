8f9289c7a0b7ad32614eb0e7f20655e3954735b9
Grigor Yankov <yankovweb@outlook.com>
2023-03-24 18:35:53 +0200

    [Future]LoginView

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import React from "react";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import Login from "./components/Login";
+// import Home from "./components/Home";
+// import Img from "./pages/Img";
+// import Root from "./pages/Root";
+// import Chat from "./pages/Chat";
+// import ErrorPage from "./pages/ErrorPage";
 
-function App() {
-  return <h1>hello</h1>;
-}
+import Root from "./routes/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [{path: "/login", element: <Login />}],
+    // errorElement: <ErrorPage />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;

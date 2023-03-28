a46cbb800cbc99155cf1116c3ecaa85e819a0532
Grigor Yankov <yankovweb@outlook.com>
2023-03-28 18:29:53 +0300

    [Futre]HomePage

--- my-app/src/components/HomePreview/HomeComponent.js
+++ my-app/src/components/HomePreview/HomeComponent.js
@@ -1,5 +1,69 @@
+import React from "react";
+import {styled} from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+// Define the styles for the different elements using the `styled` function
+const Header = styled(Box)({
+  height: "100px",
+  backgroundColor: "#00bcd4",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const Title = styled(Typography)({
+  color: "#fff",
+  fontWeight: "bold",
+  fontSize: "2rem",
+});
+
+const Content = styled(Box)({
+  height: "calc(100vh - 100px)",
+  backgroundColor: "#f5f5f5",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const Subtitle = styled(Typography)({
+  color: "#333",
+  fontWeight: "bold",
+  fontSize: "1.5rem",
+  opacity: 0,
+  transition: "opacity 1s ease-in-out",
+  "&.fade-in": {
+    opacity: 1,
+  },
+});
+
 const HomeComponent = () => {
-  return <div className="home">Welcome</div>;
+  const [isSubtitleVisible, setIsSubtitleVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    // Set the `isSubtitleVisible` state to true after a delay
+    const timeoutId = setTimeout(() => {
+      setIsSubtitleVisible(true);
+    }, 1000);
+
+    // Clear the timeout if the component unmounts or the `isSubtitleVisible` state is set to true
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <>
+      <Header>
+        <Title>Welcome to our Educational School</Title>
+      </Header>
+      <Content>
+        <Subtitle className={isSubtitleVisible ? "fade-in" : ""}>
+          Learn new skills and advance your career with us
+        </Subtitle>
+      </Content>
+    </>
+  );
 };
 
 export default HomeComponent;

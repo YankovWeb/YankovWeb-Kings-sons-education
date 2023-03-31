cb3dc773d7f721f5a8c15a97387c6005f1cf220b
Grigor Yankov <yankovweb@outlook.com>
2023-04-01 02:13:01 +0300

    [Future]HomePage

--- my-app/src/components/HomePreview/HomeComponent.js
+++ my-app/src/components/HomePreview/HomeComponent.js
@@ -49,7 +49,7 @@ const HomeComponent = () => {
     // Set the `isSubtitleVisible` state to true after a delay
     const timeoutId = setTimeout(() => {
       setIsSubtitleVisible(true);
-    }, 1500);
+    }, 500);
 
     // Clear the timeout if the component unmounts or the `isSubtitleVisible` state is set to true
     return () => {

c7bb469eceea46ba62ca6cdf10825b5b6f42969c
Grigor Yankov <yankovweb@outlook.com>
2023-03-31 21:01:35 +0300

    [Future]ReadAll

--- my-app/src/hooks/useProducts.js
+++ my-app/src/hooks/useProducts.js
@@ -1,9 +1,112 @@
+import {useState, useEffect} from "react";
+import {collection, doc, onSnapshot, deleteDoc} from "firebase/firestore";
+import {db} from "../config/firebase";
 import {
-  createProdutItem,
-  updateCollectionItem,
-  deleteColectionItem,
-} from "../serivces/products";
+  readAllProducts,
+  transformObjectToArrayWithId,
+} from "../serivces/products/productService";
 
-const useProducts = () => {};
+const useProducts = () => {
+  const [allProducts, setAllProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const collectionName = "products";
+
+  const readAll = async () => {
+    setLoading(true);
+    try {
+      const response = await readAllProducts(db, collectionName);
+      const dataArray = transformObjectToArrayWithId(response);
+      setAllProducts(dataArray);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  const readOne = async (id) => {
+    setLoading(true);
+    try {
+      const docRef = doc(db, collectionName, id);
+      const docSnapshot = await docRef.get();
+      const data = {id: docSnapshot.id, ...docSnapshot.data()};
+      setLoading(false);
+      return data;
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  const createOne = async (data) => {
+    setLoading(true);
+    try {
+      const docRef = await collection(db, collectionName).add(data);
+      const newData = {id: docRef.id, ...data};
+      setAllProducts([...allProducts, newData]);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  const updateOne = async (id, updatedData) => {
+    setLoading(true);
+    try {
+      const docRef = doc(db, collectionName, id);
+      await docRef.update(updatedData);
+      const updatedArray = allProducts.map((item) =>
+        item.id === id ? {...item, ...updatedData} : item
+      );
+      setAllProducts(updatedArray);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  const deleteOne = async (id) => {
+    setLoading(true);
+    try {
+      await deleteDoc(doc(db, collectionName, id));
+      const filteredArray = allProducts.filter((item) => item.id !== id);
+      setAllProducts(filteredArray);
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, collectionName),
+      (querySnapshot) => {
+        const dataArray = [];
+        querySnapshot.forEach((doc) => {
+          dataArray.push({id: doc.id, ...doc.data()});
+        });
+        setAllProducts(dataArray);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  return {
+    allProducts,
+    loading,
+    error,
+    readAll,
+    readOne,
+    createOne,
+    updateOne,
+    deleteOne,
+  };
+};
 
 export default useProducts;
--- my-app/src/serivces/products/productService.js
+++ my-app/src/serivces/products/productService.js
@@ -1,3 +1,4 @@
+import {async} from "@firebase/util";
 import {
   addDoc,
   collection,
@@ -5,6 +6,18 @@ import {
   doc,
   deleteDoc,
 } from "firebase/firestore";
+
+export const transformObjectToArrayWithId = (response) => {
+  const arrayWithId = Object.entries(response).map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return arrayWithId;
+};
+
+export const readAllProducts = async (db, collectionName) => {
+  return await collection(db, collectionName).get();
+};
 
 export const createProdutItem = async (db, collectionName, newData) => {
   await addDoc(collection(db, collectionName), newData);

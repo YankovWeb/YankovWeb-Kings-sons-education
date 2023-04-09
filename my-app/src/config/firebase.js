8a3d684a5e64178a8a0242ffa79768d603e9f44c
Grigor Yankov <109748669+YankovWeb@users.noreply.github.com>
2023-04-09 06:01:10 +0300

    Update firebase.js

--- my-app/src/config/firebase.js
+++ my-app/src/config/firebase.js
@@ -8,15 +8,7 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAG-ztMSvOVIBKGkUlZbJLG37hhBFB-2bA",
-  authDomain: "kse-app-64529.firebaseapp.com",
-  projectId: "kse-app-64529",
-  storageBucket: "kse-app-64529.appspot.com",
-  messagingSenderId: "470287680113",
-  appId: "1:470287680113:web:5a0947f3618e4b0240d1d0",
-  measurementId: "G-S62PFHY09B",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
